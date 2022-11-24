00d14bca185be637552e1c1da4f1738dfa20cc99
Rick van Ham <428294@student.fontys.nl>
2022-11-24 11:00:35 +0100

    Added Whirlpool that spins the dolphin

--- Assets/Scripts/Rotation/RotateAroundItself.cs
+++ Assets/Scripts/Rotation/RotateAroundItself.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class RotateAroundItself : MonoBehaviour, IRotatable
 {
     [SerializeField] private float rotationSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
