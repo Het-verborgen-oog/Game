c30cb30ee7900785e630e5dd71caaa91b3589228
tjerk-zeilstra <lala@pepr.nl>
2022-11-25 09:49:03 +0100

    avoid colisions

--- Assets/NewMovement/scripts/DolphinMovement.cs
+++ Assets/NewMovement/scripts/DolphinMovement.cs
@@ -7,15 +7,34 @@ public class DolphinMovement : MonoBehaviour
 {
     [SerializeField] public float horizontalMovmentSpeed = 5;
     [SerializeField] public float verticalMovmentSpeed = 5;
+    [SerializeField] public float raycastAngle = 45;
+    [SerializeField] public float raycastLength = 5;
+    [SerializeField] public float raycastFarLength = 6;
+    [SerializeField] public float speedMultiplier = 2;
 
     private float horizontalInput;
     private float verticalInput;
+    private Vector3 leftRay;
+    private Vector3 rightRay;
+    private Vector3 upRay;
+    private Vector3 downRay;
 
+    private void Start()
+    {
+        leftRay = Quaternion.AngleAxis(-raycastAngle, transform.up) * transform.forward;
+        rightRay = Quaternion.AngleAxis(raycastAngle, transform.up) * transform.forward;
+        upRay = Quaternion.AngleAxis(-raycastAngle, transform.right) * transform.forward;
+        downRay = Quaternion.AngleAxis(raycastAngle, transform.right) * transform.forward;
+    }
     // Update is called once per frame
     void Update()
     {
+        //gets input
         GetInput();
+        //changes horizonatal input or verticalinput if the raycasts hit somethings
+        LockMovement();
         AvoidColisions();
+        //moves the player acording to the inputs
         Movement();
     }
 
@@ -29,6 +48,8 @@ public class DolphinMovement : MonoBehaviour
         }
         else
         {
+
+            //up = -1 and down = 1
             horizontalInput = Input.GetAxis("Horizontal");
             verticalInput = Input.GetAxis("Vertical");
         }
@@ -52,16 +73,72 @@ public class DolphinMovement : MonoBehaviour
 
     private void AvoidColisions()
     {
-        
+        if (Physics.Raycast(transform.position, leftRay, raycastLength))
+        {
+            Debug.Log("left Ray");
+            horizontalInput = 1 * speedMultiplier;
+        }
+
+        if (Physics.Raycast(transform.position, rightRay, raycastLength))
+        {
+            Debug.Log("right Ray");
+            horizontalInput = -1f * speedMultiplier;
+        }
+
+        if (Physics.Raycast(transform.position, upRay, raycastLength))
+        {
+            Debug.Log("up Ray");
+            verticalInput = 1f * speedMultiplier;
+        }
+
+        if (Physics.Raycast(transform.position, downRay, raycastLength))
+        {
+            Debug.Log("down Ray");
+            verticalInput = -1f * speedMultiplier;
+        }
+    }
+
+    private void LockMovement()
+    {
+        if (Physics.Raycast(transform.position, leftRay, raycastFarLength))
+        {
+            if (horizontalInput < 0)
+            {
+                horizontalInput = 0;
+            }
+        }
+
+        if (Physics.Raycast(transform.position, rightRay, raycastFarLength))
+        {
+            if (horizontalInput > 0)
+            {
+                horizontalInput = 0;
+            }
+        }
+
+        if (Physics.Raycast(transform.position, upRay, raycastFarLength))
+        {
+            if (verticalInput < 0)
+            {
+                verticalInput = 0;
+            }
+        }
+
+        if (Physics.Raycast(transform.position, downRay, raycastFarLength))
+        {
+            if (verticalInput > 0)
+            {
+                verticalInput = 0;
+            }
+        }
     }
 
     private void OnDrawGizmos()//used to see Ray in editor without update function
     {
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10, Color.red);
-        Vector3 leftRay = Quaternion.AngleAxis(45, transform.up) * transform.forward;
-        Vector3 rightRay = Quaternion.AngleAxis(-45, transform.up) * transform.forward;
-        
-        Debug.DrawRay(transform.position, leftRay * 10, Color.red);
-        Debug.DrawRay(transform.position, rightRay * 10, Color.red);
+        //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10, Color.red);
+        Debug.DrawRay(transform.position, leftRay * raycastLength, Color.red);
+        Debug.DrawRay(transform.position, rightRay * raycastLength, Color.red);
+        Debug.DrawRay(transform.position, upRay * raycastLength, Color.green);
+        Debug.DrawRay(transform.position, downRay * raycastLength, Color.green);
     }
 }
