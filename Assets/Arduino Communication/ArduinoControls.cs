0ad0d037552affb6626f17d07e62a1862fc885d8
Nathan Thus <nathan.thus@outlook.com>
2022-12-05 11:16:08 +0100

    Update ArduinoControls.cs
    Changed values to represent the measurements taken on-site.

--- Assets/Arduino Communication/ArduinoControls.cs
+++ Assets/Arduino Communication/ArduinoControls.cs
@@ -30,8 +30,17 @@ public class ArduinoControls : MonoBehaviour, IArduinoData
 
     const float MinimumPitch = -1f, MaximumPitch = 1f, MinimumSpeed = 1f, MaximumSpeed = 2.5f;
     const float MinimumRoll = -1f, MaximumRoll = 1f;
-    const float ExpectedMinimumPitch = 0f, ExpectedMaximumPitch = 1024f, ExpectedMinumumSpeed = 0f, ExpectedMaximumSpeed = 5f;
-    const float ExpectedMinimumRoll = 0f, ExpectedMaximumRoll = 1024f;
+
+    //These values were calculated with rigourous testing :)
+    // JK I wrote a program that calculated all digital values the arduino can output and their respective voltages, and used that as a lookup table :)
+    const float
+        ExpectedMinimumPitch = 82f,
+        ExpectedMaximumPitch = 409f,
+        ExpectedMinumumSpeed = 0f,
+        ExpectedMaximumSpeed = 5f;
+    const float 
+        ExpectedMinimumRoll = 511f,
+        ExpectedMaximumRoll = 736f;
 
     public float Roll { get { return (keyValuePairs["HRZ"] - ExpectedMinimumRoll) / (ExpectedMaximumRoll - ExpectedMinimumRoll) * (MaximumRoll - MinimumRoll) + MinimumRoll; } }
 
