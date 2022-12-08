fac02e434b53e1edb45c036576f6e973cff50908
Nathan Thus <nathan.thus@outlook.com>
2022-12-08 15:14:12 +0100

    Fixed some Score related issues

--- Assets/Objective/Score Manager/ScoreManager.cs
+++ Assets/Objective/Score Manager/ScoreManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public static class ScoreManager
@@ -8,7 +9,7 @@ public static class ScoreManager
     public static void Add(int amount)
     {
         Score += amount;
-        Debug.Log($"Score: {Score}");
+        GameObject.Find("Score").GetComponent<TextMeshPro>().text = "Score: " + Score;
     }
 
     public static void Reset()
