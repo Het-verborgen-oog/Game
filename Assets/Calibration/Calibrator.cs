adaeaaab92dff00fc1b23e67498b44e64e076c7f
Nathan Thus <nathan.thus@outlook.com>
2022-12-22 13:14:04 +0100

    Update Calibrator.cs
    Can now display values

--- Assets/Calibration/Calibrator.cs
+++ Assets/Calibration/Calibrator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -29,13 +30,68 @@ public class Calibrator : MonoBehaviour
     [SerializeField]
     ArduinoControls.MeasureDataIndex RequestedData;
 
+    private enum MeasureState
+    { 
+        Idle = 0,
+        Minimum = 1,
+        Maximum = 2
+    }
+
+    MeasureState CurrentState = MeasureState.Idle;
+    float lastArduinoValue;
+    bool lastInputConfirmed;
+
+    private void Start()
+    {
+    }
+
     void Update()
     {
-        MinimumSlider.value = arduino.DataCollection[(int)RequestedData].InputMinimum;
-        MinimumText.text = MinimumSlider.value.ToString();
+        if (CurrentState != MeasureState.Idle)
+        {
+            MinimumSlider.value = arduino.DataCollection[(int)RequestedData].InputMinimum;
+            MinimumText.text = MinimumSlider.value.ToString();
 
-        MaximumSlider.value = arduino.DataCollection[(int)RequestedData].InputMaximum;
-        MaximumText.text = MaximumSlider.value.ToString();
+            MaximumSlider.value = arduino.DataCollection[(int)RequestedData].InputMaximum;
+            MaximumText.text = MaximumSlider.value.ToString();
+        }
+        
+        ValueText.text = arduino.GrabRawProperty(RequestedData).ToString();
+    }
 
+    public void Calibrate()
+    {
+        switch (CurrentState)
+        {
+            case MeasureState.Idle:
+                MinimumCalibration();
+                CurrentState = MeasureState.Minimum;
+                break;
+            case MeasureState.Minimum:
+                MaximumCalibration();
+                CurrentState = MeasureState.Maximum;
+                break;
+            case MeasureState.Maximum:
+                SaveData();
+                CurrentState = MeasureState.Idle;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void MaximumCalibration()
+    {
+        lastArduinoValue = arduino.GrabRawProperty(RequestedData);
+    }
+
+    private void SaveData()
+    {
+        Debug.Log("Saving!");
+    }
+
+    private void MinimumCalibration()
+    {
+        
     }
 }
